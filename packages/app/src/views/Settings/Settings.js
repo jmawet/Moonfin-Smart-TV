91edaf8d3c273c40655f07e3b099e3b376cee127
Jennifer Dupaquier <11723765+jmawet@users.noreply.github.com>
2026-07-08 10:47:00 -0700

    Update Settings.js

--- packages/app/src/views/Settings/Settings.js
+++ packages/app/src/views/Settings/Settings.js
@@ -323,8 +323,8 @@ const getImageTypeOptions = () => [
 ];
 
 const getHomeRowsStyleOptions = () => [
-	{ value: 'modern', label: $L('Modern') },
-	{ value: 'classic', label: $L('Classic') }
+	{ value: 'v2', label: $L('Modern') },
+	{ value: 'v1', label: $L('Classic') }
 ];
 
 const getHomeRowSortOptions = () => [
